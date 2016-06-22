8cd5d3ed4a8a2e887bf97392ee684161b9867fd7
smcgee31 <solodesign31@gmail.com>
2016-06-22 15:11:38 -0600

    finished the tutorial

---
+++ src/components/SingleResult.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class SingleResult extends React.Component {
+  render() {
+    return (
+      <a href={this.props.url} className="single-result" target="_blank">
+        <div>
+          <h3>{this.props.title}</h3>
+          <p>{this.props.description}</p>
+        </div>
+      </a>
+    )
+  }
+}
+
+
+// DONE
