3d7b8330340edce700369c1301f313a4c79fcf28
smcgee31 <solodesign31@gmail.com>
2016-06-23 13:27:54 -0600

    setting up for hosting

---
+++ server/index.js
@@ -0,0 +1,10 @@
+// React Wikipedia Search
+var express = require( 'express' );
+var app = express();
+var port = 8000;
+
+app.use(express.static(__dirname));
+
+app.listen( port, function () {
+	console.log( 'listening on port ', port );
+} );
